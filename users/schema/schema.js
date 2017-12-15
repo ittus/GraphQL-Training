86be7379cb33f13fa5ceaf1efcb1e58986487341
Vu Minh Thang <vuminhthang.cm@gmail.com>
2017-12-15 19:49:06 +0900

    Reference from Company->User

--- users/schema/schema.js
+++ users/schema/schema.js
@@ -5,7 +5,8 @@ const {
   GraphQLObjectType,
   GraphQLString,
   GraphQLInt,
-  GraphQLSchema
+  GraphQLSchema,
+  GraphQLList
 } = graphql
 
 const users = [
@@ -23,16 +24,23 @@ const users = [
 
 const CompanyType = new GraphQLObjectType({
   name: 'Company',
-  fields : {
+  fields : () => ({
     id: { type: GraphQLString },
     name: { type: GraphQLString },
-    description: { type: GraphQLString }
-  }
+    description: { type: GraphQLString },
+    users: {
+      type: new GraphQLList(UserType),
+      resolve (parentValue, args) {
+        return axios.get(`http://localhost:3000/companies/${parentValue.id}/users`)
+          .then(res => res.data)
+      }
+    }
+  })
 })
 
 const UserType = new GraphQLObjectType({
   name: 'User',
-  fields: {
+  fields: () => ({
     id: { type: GraphQLString},
     firstName: { type: GraphQLString },
     age: { type: GraphQLInt },
@@ -43,7 +51,7 @@ const UserType = new GraphQLObjectType({
           .then(response => response.data)
       }
     }
-  }
+  })
 })
 
 const RootQuery = new GraphQLObjectType({
