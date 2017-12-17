5670ccfc06ef2c63d61ffb64338a159db100f9a9
Vu Minh Thang <vuminhthang.cm@gmail.com>
2017-12-17 21:30:10 +0900

    Add deleteSong mutation

--- Lyrical-GraphQL/client/components/SongList.js
+++ Lyrical-GraphQL/client/components/SongList.js
@@ -5,11 +5,22 @@ import { Link } from 'react-router';
 import query from '../query/fetchSongs';
 
 class SongList extends Component {
+  onSongDelete(id) {
+    this.props.mutate({
+      variables: {
+        id
+      }
+    }).then(() => this.props.data.refetch())
+  }
+
   renderSong() {
-    return this.props.data.songs.map(song => {
+    return this.props.data.songs.map(({id, title}) => {
       return (
-        <li key={song.id} className="collection-item">
-          {song.title}
+        <li key={id} className="collection-item">
+          {title}
+          <i className="material-icons" onClick={() => this.onSongDelete(id)}>
+            delete
+          </i>
         </li>
       )
     })
@@ -35,4 +46,14 @@ class SongList extends Component {
   }
 }
 
-export default graphql(query)(SongList);
+const mutation = gql`
+  mutation DeleteSong($id: ID) {
+    deleteSong(id: $id) {
+      id
+    }
+  }
+`;
+
+export default graphql(mutation)(
+  graphql(query)(SongList)
+);
