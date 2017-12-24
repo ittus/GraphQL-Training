9260ad1c4b31d7a0828695eb0255e2e523272f21
ittus <vuminhthang.cm@gmail.com>
2017-12-24 14:51:17 +0700

    Set up header base on authentication state

--- auth-graphql/client/components/Header.js
+++ auth-graphql/client/components/Header.js
@@ -1,13 +1,49 @@
 import React, {Component} from 'react';
+import { graphql } from 'react-apollo';
+import { Link } from 'react-router';
+import query from '../queries/CurrentUser';
 
 class Header extends Component {
+  renderButtons () {
+    const { loading, user } = this.props.data;
+    if (this.props.data.loading) {
+      return <div/>;
+    }
+    if (user) {
+      return (
+        <div>Logout</div>
+      )
+    } else {
+      return (
+        <div>
+          <li>
+            <Link to="/signup">Sign up</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+        </div>
+      )
+    }
+
+  }
+
   render() {
+    console.log(this.props.data);
     return (
-      <div>
-        Header
-      </div>
+      <nav>
+        <div className="nav-wrapper">
+          <Link to="/" className="brand-logo left">
+            Home
+          </Link>
+          <ul className="right">
+            {this.renderButtons()}
+          </ul>
+
+        </div>
+      </nav>
     )
   }
 }
 
-export default Header;
+export default graphql(query)(Header) ;
